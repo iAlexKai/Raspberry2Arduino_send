4fa16a5591fe924ab4274b683744c7c8a010de6c
Your Name <you@example.com>
2018-09-14 18:26:26 +0800

    delete some annotations

--- examples_linux/gettingstarted.cpp
+++ examples_linux/gettingstarted.cpp
@@ -150,49 +150,6 @@ int main(int argc, char** argv){
 			printf("Have sent: %s\n", msg2send);
 			sleep(1);
 
-		/*	// Take the time, and send it.  This will block until complete
-
-			printf("Now sending...\n");
-			unsigned long time = millis();
-
-			bool ok = radio.write( &time, sizeof(unsigned long) );
-
-			if (!ok){
-				printf("failed.\n");
-			}
-			// Now, continue listening
-			radio.startListening();
-
-			// Wait here until we get a response, or timeout (250ms)
-			unsigned long started_waiting_at = millis();
-			bool timeout = false;
-			while ( ! radio.available() && ! timeout ) {
-				if (millis() - started_waiting_at > 200 )
-					timeout = true;
-			}
-
-
-			// Describe the results
-			if ( timeout )
-			{
-				printf("Failed, response timed out.\n");
-			}
-			else
-			{
-				// Grab the response, compare, and send to debugging spew
-				unsigned long got_time;
-				radio.read( &got_time, sizeof(unsigned long) );
-
-				// Spew it
-				printf("Got response %lu, round-trip delay: %lu\n",got_time,millis()-got_time);
-			}
-			sleep(1);
-*/
-		}
-
-		//
-		// Pong back role.  Receive each packet, dump it out, and send it back
-		//
 
 		if ( role == role_pong_back )
 		{
